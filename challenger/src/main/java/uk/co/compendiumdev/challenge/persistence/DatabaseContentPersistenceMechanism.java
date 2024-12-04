4f6044e85aeb27761aa1a137671d7118b7e38205
JC-Bees <126421484+JC-Bees@users.noreply.github.com>
2024-12-04 13:03:57 -0500

    Delete challenger directory

--- challenger/src/main/java/uk/co/compendiumdev/challenge/persistence/DatabaseContentPersistenceMechanism.java
+++
@@ -1,9 +0,0 @@
-package uk.co.compendiumdev.challenge.persistence;
-
-import uk.co.compendiumdev.thingifier.core.domain.instances.ERInstanceData;
-
-public interface DatabaseContentPersistenceMechanism {
-
-    PersistenceResponse saveDatabaseContent(String guid, ERInstanceData instanceCollection);
-    PersistenceResponse loadDatabaseContent(String guid);
-}
